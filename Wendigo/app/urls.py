f8ea903277d8b01d8b81dee88ce0f675997b541f
Harri Siva <harrishiv6@gmail.com>
2022-01-05 22:45:18 -0500

    added the results as a view, in URL and fixed the result and result.css relationship by loading static and adding the right css path

--- Wendigo/app/urls.py
+++ Wendigo/app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # Results Page
 
     path("", views.homepage_view, name="homepage"),
+    path("result",views.results_view,name="result"), # result will call the path after this and populate it with info?
     path("<str:tweet_id>", views.index, name="index"), # if we get into this app and if we are in the home page, go to the views.index page that has the name index
 ]
