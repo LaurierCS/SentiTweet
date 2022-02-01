e7182ce6ba53337010f037d3cffe2e8163c8d8cb
Harri Siva <harrishiv6@gmail.com>
2022-02-01 16:19:15 -0500

    Users can now attach the link of the tweet and the ID will be extracted

--- Wendigo/app/views.py
+++ Wendigo/app/views.py
@@ -22,7 +22,13 @@ def homepage_view(response):
     if response.method == "POST":
         form = CreateQuery(response.POST)
         if form.is_valid():
-            tweet_id = form.cleaned_data["query"]
+            tweet_url = form.cleaned_data["query"]
+            tweet_id = tweet_url.split('/')[-1] #Extract the ID from the URL
+            # Check if the tweet_id is valid, if not thorw an error message
+            if (len(id)==19 and id.isnumeric()): #Conditions for a valid ID
+                return HttpResponseRedirect("/result/%s" %tweet_id) # redirect to the right URL and create an entry with the model
+            
+            # else throw an error message on the HTML page saying its invalid
             return HttpResponseRedirect("/result/%s" %tweet_id) 
     else:
         form = CreateQuery() # Creates a blank form
