da2bde8e9287baf50f9f51e4c68e6327e5974f6d
Harri Siva <harrishiv6@gmail.com>
2022-02-10 16:36:05 -0500

    Added function that uses google trends and creates a list of 20 trending topics with a twitter link to point to be used in an href, this is for the homepage

--- Wendigo/app/functions.py
+++ Wendigo/app/functions.py
@@ -1,6 +1,11 @@
 from nltk import tokenize, FreqDist
+from pytrends.request import TrendReq
 
 def wordCloud(sent): #NOTE: Takes a sentence and returns the most frequent words for the word cloud
     words = tokenize.word_tokenize(sent) #tokenize the sentence
     freq = [s[0] for s in FreqDist(words).most_common() if s[0].isalpha()] # sort by occurance and filter by alphabets
     return freq # return type is list
+
+def getTrendList():
+    # returns a 2D list where item 0 is a string of the trending topic and item at index 1 is the link to be used as an HREF
+    return [[val,"https://twitter.com/hashtag/{}".format(val.replace(" ",""))] for val in TrendReq().trending_searches(pn='united_states')[0]] # get the top 20 google trends
